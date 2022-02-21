fb15f719810a83f5df4b9eb103123e5ec778ccda
Christian <christian.rust@gmx.com>
2022-02-21 15:29:05 +0100

    changed to vue 2

--- vue-cli/vue.config.js
+++ vue-cli/vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir:"../../../public/plugin_assets/redmine_super_gantt/javascripts",
