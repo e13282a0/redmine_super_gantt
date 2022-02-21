1950dee5037528e02c342131eb1d7e010fe30362
Christian <christian.rust@gmx.com>
2022-02-21 12:06:22 +0100

    routes and menu initialized

--- init.rb
+++ init.rb
@@ -1,8 +1,11 @@
 Redmine::Plugin.register :redmine_super_gantt do
   name 'Redmine Super Gantt plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Christian Rust'
+  description 'This is an improved gantt plugin for redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/e13282a0/redmine_gantt/'
+  author_url 'https://github.com/e13282a0'
+  menu :application_menu, :super_gantt, { controller: 'super_gantt', action: 'index' }, caption: 'Super Gantt'
+  #permission :super_gantt, { :super_gantt => [:index] }, :public => true
+  #menu :project_menu, :super_gantt, { controller: 'super_gantt', action: 'index' }, caption: 'Super Gantt', :param => :project_id
 end
