3b7f0150a38c39bedf72d7350578841e73ec1900
Christian <christian.rust@gmx.com>
2022-02-21 15:03:52 +0100

    vue workflow running

--- vue-cli/vue.config.js
+++ vue-cli/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir:"../assets",
+  outputDir:"../../../public/plugin_assets/redmine_super_gantt/javascripts",
   filenameHashing:false,
 });
