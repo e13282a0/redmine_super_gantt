b1c4dc8918158d52e1f63d013e426b10bb10c61a
Christian <christian.rust@gmx.com>
2022-03-20 10:41:19 +0100

    milestone appearance

--- vue-cli/src/components/gantt/GanttTimeLine.vue
+++ vue-cli/src/components/gantt/GanttTimeLine.vue
@@ -1,22 +1,20 @@
 <template>
   <div>
-    <div
-      v-for="milestone in milestones"
-      :style="getCssVars(milestone.effective_date)"
-      class="milestone"
-      :key="milestone.name"
-      draggable
-      @dragstart="startDrag($event, milestone)"
-    >
-      o {{ milestone.name }}
-    </div>
+    <template v-for="milestone in milestones"  >
+      <div :key="milestone.name" draggable @dragstart="startDrag($event, milestone)" class="milestone" :style="getCssVars(milestone.effective_date)">
+        <v-icon small>mdi-rhombus</v-icon>
+      </div>
+      <div class="milestone-caption" :key="'caption' + milestone.name" :style="getCssVars(milestone.effective_date)">
+        {{ milestone.name }}
+      </div>
+    </template>
     <!--<div class="signBar" :style="barCssVars" />-->
   </div>
 </template>
 
 <script>
-import moment from 'moment'
-import {mapState, mapGetters} from 'vuex'; 
+import moment from "moment";
+import { mapState, mapGetters } from "vuex";
 export default {
   //drag drop example
   //https://learnvue.co/2020/01/how-to-add-drag-and-drop-to-your-vuejs-project/#adding-drag-and-drop-functionality
@@ -30,16 +28,16 @@ export default {
     };
   },
   computed: {
-    ...mapState(['timeBeam']),
-    ...mapGetters(['getTimeBeamIndexByDate', 'getTimeBeamPositionByDate']),
+    ...mapState(["timeBeam"]),
+    ...mapGetters(["getTimeBeamIndexByDate", "getTimeBeamPositionByDate"]),
   },
   methods: {
     getCssVars: function (date) {
-      let elm = this.getTimeBeamPositionByDate(moment(date).startOf('day').hours(12), this.colWidth);
+      let elm = this.getTimeBeamPositionByDate(moment(date).startOf("day").hours(12), this.colWidth);
       // eslint-disable-next-line
       // debugger;
       return {
-        "--left": (elm.index * this.colWidth) + elm.offset -3 + "px", // subtract 3 px for font width of mark
+        "--left": elm.index * this.colWidth + elm.offset - 3 + "px", // subtract 3 px for font width of mark
         "--height": this.rowHeight - 3 + "px",
       };
     },
@@ -57,22 +55,32 @@ export default {
   z-index: 50;
   display: inline;
   cursor: move;
-  top: 5px;
+  top: 1px;
   left: var(--left);
   height: var(--height);
   /*width: var(--colWidth);*/
   line-height: var(--height);
   position: absolute;
-  width:200px;
+  vertical-align: middle;
+  font-size: xx-small;
+}
+
+.milestone-caption {
+  z-index: 50;
+  display: inline;
+  top: 1px;
+  left: var(--left);
+  height: var(--height);
+  /*width: var(--colWidth);*/
+  margin-left:2em;
+  line-height: var(--height);
+  position: absolute;
+  width: 200px;
   vertical-align: middle;
   font-size: xx-small;
   transform-origin: left bottom;
   transform: rotate(-5deg);
 }
-
-
-
-
 
 .signBar {
   position: absolute;
@@ -83,6 +91,5 @@ export default {
   left: var(--left);
   height: 4px;
   /*width: var(--width);*/
-  
 }
 </style>
